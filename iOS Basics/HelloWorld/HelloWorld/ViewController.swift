9b98115f4b9deec48c839e5131a45cc5516ee3fd
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-23 20:31:06 +0200

    Label IBOutlet

--- iOS Basics/HelloWorld/HelloWorld/ViewController.swift
+++ iOS Basics/HelloWorld/HelloWorld/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        helloWorldLabel.text = "Hello Hunters! 🔥"
+        helloWorldLabel.backgroundColor = .clear
     }
 
 
