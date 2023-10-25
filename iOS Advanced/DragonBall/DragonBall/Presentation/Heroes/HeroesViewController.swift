f1328668e39ae621479de4d42ffcbd75dddd4184
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-25 21:33:17 +0200

    HeroesView: Innitial structure

---
+++ iOS Advanced/DragonBall/DragonBall/Presentation/Heroes/HeroesViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HeroesViewController.swift
+//  DragonBall
+//
+//  Created by Daniel Cazorro Frías on 25/10/23.
+//
+
+import UIKit
+
+
+class HeroesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ iOS Advanced/DragonBall/DragonBall/Presentation/Heroes/HeroesViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HeroesViewModel.swift
+//  DragonBall
+//
+//  Created by Daniel Cazorro Frías on 25/10/23.
+//
+
+import Foundation
+
+class HeroesViewModel {
+    
+}
