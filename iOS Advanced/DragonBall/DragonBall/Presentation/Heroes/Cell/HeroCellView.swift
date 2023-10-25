b34908a8ea77e2d16d04cb185d5720733183e598
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-25 21:57:58 +0200

    TableViewCell Heroes

---
+++ iOS Advanced/DragonBall/DragonBall/Presentation/Heroes/Cell/HeroCellView.swift
@@ -0,0 +1,21 @@
+//
+//  HeroCellView.swift
+//  DragonBall
+//
+//  Created by Daniel Cazorro Frías on 25/10/23.
+//
+
+import UIKit
+
+class HeroCellView: UITableViewCell {
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+    }
+    
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
