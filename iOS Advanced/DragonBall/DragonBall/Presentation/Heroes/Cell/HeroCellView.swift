17fd514686a2a783b8cb7f9a1d3a067ca801b83b
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-25 22:07:15 +0200

    Moving cell: To a different xib

--- iOS Advanced/DragonBall/DragonBall/Presentation/Heroes/Cell/HeroCellView.swift
+++ iOS Advanced/DragonBall/DragonBall/Presentation/Heroes/Cell/HeroCellView.swift
@@ -9,13 +9,25 @@ import UIKit
 
 class HeroCellView: UITableViewCell {
     
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var heroeDescription: UILabel!
+    
     override func prepareForReuse() {
         super.prepareForReuse()
         
+        name.text = nil
+        photo.image = nil
+        heroeDescription.text = nil
     }
     
     override class func awakeFromNib() {
         super.awakeFromNib()
+        
+    }
+    
+    func updateView() {
         
     }
 }
