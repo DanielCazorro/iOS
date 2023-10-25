d990044fc907a7f7573fe6a680f3b17febbf0380
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-25 10:03:02 +0200

    ApiProvider

---
+++ iOS Advanced/DragonBall/DragonBall/Data/Api/ApiProvider.swift
@@ -0,0 +1,17 @@
+//
+//  ApiProvider.swift
+//  DragonBall
+//
+//  Created by Daniel Cazorro Frías on 25/10/23.
+//
+
+import Foundation
+
+
+class ApiProvider {
+    static private let apiBaseURL = "https://dragonball.keepcoding.education/api"
+    
+    func login(for user: String, with password: String) {
+        
+    }
+}
