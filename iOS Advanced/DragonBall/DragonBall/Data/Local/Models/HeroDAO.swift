a1d7622516700bb6732ab445e441bfb76e6ff96f
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-26 10:28:39 +0200

    Core Data Hero and location

---
+++ iOS Advanced/DragonBall/DragonBall/Data/Local/Models/HeroDAO.swift
@@ -0,0 +1,14 @@
+//
+//  HeroDAO.swift
+//  DragonBall
+//
+//  Created by Daniel Cazorro Frías on 26/10/23.
+//
+
+import Foundation
+import CoreData
+
+@objc(HeroDAO)
+class HeroDAO: NSManagedObject {
+    @NSManaged var id: String?
+}
