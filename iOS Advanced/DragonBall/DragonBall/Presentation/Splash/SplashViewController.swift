2571746165a042d06448b5f30affdb1ad08040a4
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-27 18:49:43 +0200

    Preparing splash view

--- iOS Advanced/DragonBall/DragonBall/Presentation/Splash/SplashViewController.swift
+++ iOS Advanced/DragonBall/DragonBall/Presentation/Splash/SplashViewController.swift
@@ -7,10 +7,57 @@
 
 import UIKit
 
+protocol SplashViewControllerDelegate {
+    var viewState: ((SplashViewState) -> Void)? { get set }
+    
+    func onViewAppear()
+}
+
+enum SplashViewState {
+    case loading(_ isLoading: Bool)
+    case navigateToLogin
+    case navigateToHeroes
+}
+
 class SplashViewController: UIViewController {
+    @IBOutlet weak var loading: UIActivityIndicatorView!
+    
+    var viewModel: SplashViewControllerDelegate?
    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setObservers()
+        viewModel?.onViewAppear()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "SPLASH_TO_LOGIN":
+            break
+            
+        case "SPLASH_TO_HEROES":
+            break
+            
+        default:
+            break
+        }
+    }
+    
+    private func setObservers() {
+        viewModel?.viewState = { [weak self] state in
+            DispatchQueue.main.async {
+                switch state {
+                case .loading(let isLoading):
+                    self?.loading.isHidden = !isLoading
+                    
+                case .navigateToLogin:
+                    self?.performSegue(withIdentifier: "SPLASH_TO_LOGIN", sender: nil)
+                    
+                case .navigateToHeroes:
+                    self?.performSegue(withIdentifier: "SPLASH_TO_HEROES", sender: nil)
+                }
+            }
+        }
     }
 }
     
--- iOS Advanced/DragonBall/DragonBall/Presentation/Splash/SplashViewModel.swift
+++ iOS Advanced/DragonBall/DragonBall/Presentation/Splash/SplashViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+class SplashViewModel: SplashViewControllerDelegate {
+    private let apiProvider: ApiProviderProtocol
+    private let secureDataProvider: SecureDataProviderProtocol
+    
+    var viewState: ((SplashViewState) -> Void)?
+    
+    private var isLogged: Bool {
+        secureDataProvider.getToken()?.isEmpty == false
+    }
+    
+    init(apiProvider: ApiProviderProtocol, secureDataProvider: SecureDataProviderProtocol) {
+        self.apiProvider = apiProvider
+        self.secureDataProvider = secureDataProvider
+    }
+    
+    func onViewAppear() {
+        viewState?(.loading(true))
+        
+        DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(1)) {
+            self.isLogged ? self.viewState?(.navigateToHeroes) : self.viewState?(.navigateToLogin)
+        }
+    }
+    
+}
