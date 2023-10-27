76775aeee7220b010f04d93ce29d6a64f3bba3eb
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-27 18:04:55 +0200

    MakingSplashView

---
+++ iOS Advanced/DragonBall/DragonBall/Presentation/Splash/SplashViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SplashViewController.swift
+//  DragonBall
+//
+//  Created by Daniel Cazorro Frías on 27/10/23.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+    
---
+++ iOS Advanced/DragonBall/DragonBall/Presentation/Splash/SplashViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SplashViewModel.swift
+//  DragonBall
+//
+//  Created by Daniel Cazorro Frías on 27/10/23.
+//
+
+import Foundation
