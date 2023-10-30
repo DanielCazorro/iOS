9883e8308cd45cc8bae5c0e23069d0a008119604
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-30 21:46:22 +0100

    Programación funcional

--- iOS Super Powers/Playground.Combine.Day1.playground/Pages/Combine-03.xcplaygroundpage/Contents.swift
+++ iOS Super Powers/Playground.Combine.Day1.playground/Pages/Combine-03.xcplaygroundpage/Contents.swift
@@ -68,5 +68,74 @@ nums
         print("Último valor: \($0)")
     }
 
+print("-----------------------------------")
+print("OPERADORES DE SECUENCIA")
+print("-----------------------------------")
+
+let publisher = [1,2,3,4,5,6,7,8,9].publisher
+
+publisher
+    .min()
+    .sink { value in
+        print("Mínimo: \(value)")
+    }
+
+publisher
+    .max()
+    .sink { value in
+        print("Máximo: \(value)")
+    }
+
+let publisher2 = ["Thor","Spiderman","Ant"].publisher
+
+//cadena menor
+publisher2
+    .min { $0.count < $1.count }
+    .sink { value in
+        print("Mínimo caracteres \(value)")
+    }
+
+//cadena mayor
+publisher2
+    .max { $0.count < $1.count }
+    .sink { value in
+        print("Máximo caracteres \(value)")
+    }
+
+//primero
+publisher
+    .first()
+    .sink { value in
+        print("Primero \(value)")
+    }
+
+//ultimo
+publisher
+    .last()
+    .sink { value in
+        print("Último \(value)")
+    }
+
+//acceso a un indice concreto
+publisher
+    .output(at: 2)
+    .sink { value in
+        print("Índice 2 valor emitido \(value)")
+    }
+
+//Acceso por indice en una rango
+publisher
+    .output(in: 2...4)
+    .sink { value in
+        print("Índice 2 al 4 valor emitido \(value)")
+    }
+
+//Count
+publisher
+    .count()
+    .sink { value in
+        print("Total de valores emitidos \(value)")
+    }
+
 
 //: [Next](@next)
