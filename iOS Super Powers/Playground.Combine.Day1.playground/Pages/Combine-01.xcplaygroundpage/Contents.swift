4cfeac7931e5d9d30038a35263749a68a2bea39e
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-30 20:30:55 +0100

    SINK y Completion

--- iOS Super Powers/Playground.Combine.Day1.playground/Pages/Combine-01.xcplaygroundpage/Contents.swift
+++ iOS Super Powers/Playground.Combine.Day1.playground/Pages/Combine-01.xcplaygroundpage/Contents.swift
@@ -1,3 +1,43 @@
-import UIKit
+//: [Previous](@previous)
 
-var greeting = "Hello, playground"
+import UIKit
+import Combine
+
+// MARK: - Publicadores
+
+
+// Ejemplo 1. Array de enteros
+
+print("------------------------------------------")
+print("Ejercicio 1. Array enteros. SINK")
+print("------------------------------------------")
+
+
+let publisher1 = [1,2,3,4].publisher
+
+publisher1.sink { data in
+    // Aquí llega el dato al suscriptor...
+    print("Ejercicio 1. Llega dato... \(data)")
+}
+
+
+print("------------------------------------------")
+print("Ejercicio 2. Array enteros. SINK y Completion")
+print("------------------------------------------")
+
+publisher1.sink { completion in
+    switch completion {
+    case .failure(let error):
+        //Error
+        print("Error \(error)")
+    case .finished:
+        //Success
+        print("Ejecutado success")
+    }
+} receiveValue: { data in
+    print("Ejercicio 2. Llega dato... \(data)")
+}
+
+
+
+//: [Next](@next)
