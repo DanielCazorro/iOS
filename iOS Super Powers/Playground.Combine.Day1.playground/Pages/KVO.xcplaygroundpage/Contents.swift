6c4dddfb50d2ae07dcb4375176f18c5fcb63ed37
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-30 20:04:02 +0100

    KVO Ejemplo básico de observador sencillo

--- iOS Super Powers/Playground.Combine.Day1.playground/Pages/KVO.xcplaygroundpage/Contents.swift
+++ iOS Super Powers/Playground.Combine.Day1.playground/Pages/KVO.xcplaygroundpage/Contents.swift
@@ -2,6 +2,22 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+//MARK: - KVO. Observadores de propiedad -
+
+// Ejemplo de observador de propiedad sencillo con Swift
+
+var name = "" {
+    willSet {
+        print("Nuevo valor futuro -> \(newValue)")
+        // keychain -> Borrar
+    }
+    
+    didSet {
+        print("Nuevo valor asignado -> \(name)")
+    }
+}
+
+name = "Luis"
+
 
 //: [Next](@next)
