a50414cc6ac2e910ef61feeffa5f406cca1e8ce3
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-31 20:26:32 +0100

    ButtonView

--- iOS Super Powers/KcSwfitUIComponents/KcSwfitUIComponents/ButtonView.swift
+++ iOS Super Powers/KcSwfitUIComponents/KcSwfitUIComponents/ButtonView.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct ButtonView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Button(action: {
+            // Acción here
+        }, label: {
+                Text("Mi primer Botón")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    .font(.title)
+                    // .cornerRadius(20) // Hasta iOS15
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+        })
+        
     }
 }
 
