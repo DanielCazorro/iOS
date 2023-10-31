d7df8935758a9e590e3b8d11e1673f898ba01c2d
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-31 19:26:58 +0100

    SwiftUI

---
+++ iOS Super Powers/KcSwfitUIComponents/KcSwfitUIComponents/KcSwfitUIComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KcSwfitUIComponentsApp.swift
+//  KcSwfitUIComponents
+//
+//  Created by Daniel Cazorro Frías on 31/10/23.
+//
+
+import SwiftUI
+
+@main
+struct KcSwfitUIComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
