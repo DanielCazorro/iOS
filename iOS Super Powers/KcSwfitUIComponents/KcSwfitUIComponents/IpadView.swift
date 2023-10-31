265eea8048ebc89c84cd78b96a06d7a3305a15af
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-31 23:00:01 +0100

    Ipad Swift UI View

---
+++ iOS Super Powers/KcSwfitUIComponents/KcSwfitUIComponents/IpadView.swift
@@ -0,0 +1,29 @@
+//
+//  IpadView.swift
+//  KcSwfitUIComponents
+//
+//  Created by Daniel Cazorro Frías on 31/10/23.
+//
+
+import SwiftUI
+
+struct IpadView: View {
+    @State private var heros = ["Goku","Krillin","Vegeta"]
+    @State private var selectedHero: String?
+    
+    var body: some View {
+        NavigationSplitView {
+            List(heros, id: \.self, selection: $selectedHero, rowContent: Text.init)
+                .navigationSplitViewColumnWidth(500)
+        } detail: {
+            // Detalle
+            
+            Text(selectedHero ?? "Selecciona un Heroe")
+        }
+
+    }
+}
+
+#Preview {
+    IpadView()
+}
