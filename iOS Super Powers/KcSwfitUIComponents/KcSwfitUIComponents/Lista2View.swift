aafa94775c2aea8d0302361559224763749a2a90
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-31 21:18:27 +0100

    UI Navigation

---
+++ iOS Super Powers/KcSwfitUIComponents/KcSwfitUIComponents/Lista2View.swift
@@ -0,0 +1,57 @@
+//
+//  Lista2View.swift
+//  KcSwfitUIComponents
+//
+//  Created by Daniel Cazorro Frías on 31/10/23.
+//
+
+import SwiftUI
+
+// Modelo de Hero
+struct Hero: Identifiable {
+    let id: UUID = UUID()
+    let name: String
+}
+
+
+struct Lista2View: View {
+    // Array de heroes
+    let heros = [
+        Hero(name: "Thor"),
+        Hero(name: "Spiderman"),
+        Hero(name: "Capitán América")
+    ]
+    
+    
+    var body: some View {
+        NavigationView {
+            
+            List {
+                ForEach(heros) { hero in
+                    
+                    NavigationLink {
+                        // Vista destino
+                        VStack {
+                            Text("Detalle de \(hero.name)")
+                        }
+                        
+                        
+                    } label: {
+                        // Celda
+                        HStack {
+                            Image(systemName: "person")
+                            Text(hero.name)
+                        }
+                        
+                    }
+                    
+                }
+            }
+            .navigationTitle("Heroes")
+        }
+    }
+}
+
+#Preview {
+    Lista2View()
+}
