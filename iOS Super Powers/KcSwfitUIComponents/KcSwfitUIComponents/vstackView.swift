127b7bc5717a45ae0937c4aedb14513ccfd47e11
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-31 19:48:16 +0100

    Example modification View

--- iOS Super Powers/KcSwfitUIComponents/KcSwfitUIComponents/vstackView.swift
+++ iOS Super Powers/KcSwfitUIComponents/KcSwfitUIComponents/vstackView.swift
@@ -11,9 +11,21 @@ struct vstackView: View {
     var body: some View {
         VStack{
             Text("Hola")
+            Spacer()
             Text("Keepcoders")
+            Spacer()
             Text("Esto es SwiftUI")
+                .font(.caption)
         }
+        // El orden de los factores afecta al producto (va por prioridad, de arriba hacia abajo)
+        // Por lo tanto, dejar el background para el final
+        .font(.largeTitle)
+        .frame(width: 350, height: 350)
+        .background(Color.blue)
+        .foregroundColor(.white)
+        // Por ejemplo, voy a modificar abajo y no va a variar nada
+        .frame(width: 450, height: 450)
+        .background(Color.yellow)
     }
 }
 
