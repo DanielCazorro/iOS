2963b1835a26cc56a0a502f9dab7561d143b57b0
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-11-02 22:34:31 +0100

    Paralelismo

--- iOS Super Powers/Playground.AsyncAwait.playground/Pages/04.Paralelismo.xcplaygroundpage/Contents.swift
+++ iOS Super Powers/Playground.AsyncAwait.playground/Pages/04.Paralelismo.xcplaygroundpage/Contents.swift
@@ -1,7 +1,32 @@
-//: [Previous](@previous)
+/*
+ 
+ Paralelismo async let
+ 
+ */
 
-import Foundation
+import UIKit
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
 
-var greeting = "Hello, playground"
 
-//: [Next](@next)
+func loadImage(index: Int) async throws -> UIImage{
+    
+    let imageURL = URL(string: "https://i.blogs.es/f7b0ed/steve-jobs/1366_2000.jpg")!
+    let request = URLRequest(url: imageURL)
+    let (data,_) = try await URLSession.shared.data(for: request)
+    print("Finalizada la carga de la imagen... \(index)")
+    return UIImage(data: data)!
+    
+}
+
+Task(priority: .medium) {
+    async let firstImage = loadImage(index: 1)
+    async let secondImage = loadImage(index: 2)
+    async let thirdImage = loadImage(index: 3)
+    
+    print("Antes de llamar")
+    
+    let _ = try await [firstImage, secondImage, thirdImage]
+    print("Despues, ya ejecutado")
+    
+}
