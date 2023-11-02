8b442087c5b9b0e6d94f36e74af533c3f873bb8e
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-11-02 21:26:41 +0100

    URL Response

--- iOS Super Powers/Playground.AsyncAwait.playground/Pages/01.Closures.xcplaygroundpage/Contents.swift
+++ iOS Super Powers/Playground.AsyncAwait.playground/Pages/01.Closures.xcplaygroundpage/Contents.swift
@@ -1,7 +1,36 @@
-//: [Previous](@previous)
-
+/*
+ 
+    Closures
+ 
+ */
 import Foundation
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
 
-var greeting = "Hello, playground"
+// Modelo de BootCamps
+struct BootCamps: Codable {
+    let id: UUID
+    let name: String
+}
 
-//: [Next](@next)
+// Definimos los typeAlias
+typealias successClosure = ([BootCamps])->Void
+typealias errorClosure = (() -> Void)?
+
+// Extensión de URLResponse
+extension URLResponse {
+    func getStatusCode() -> Int? {
+        if let httpResponse = self as? HTTPURLResponse {
+            return httpResponse.statusCode
+        }
+        return nil
+    }
+}
+
+
+final class testLoad {
+    func loadBootCamps(onSuccess: @escaping successClosure, onError: errorClosure) -> Void{
+        let url = URL(String: "https://dragonball.keepcoding.education/api/data/bootcamps")
+        let request : URLRequest = URLRequest(url: url)
+    }
+}
