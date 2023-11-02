e6d28eab1154b15661f604d45b8a0a0e87b24230
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-11-02 19:27:17 +0100

    Cell Preview

--- iOS Super Powers/kcMultiplataforma2023/kcMultiplataforma2023/EmpleadoRowView.swift
+++ iOS Super Powers/kcMultiplataforma2023/kcMultiplataforma2023/EmpleadoRowView.swift
@@ -12,11 +12,40 @@ struct EmpleadoRowView: View {
     let employee: Empleados
     
     var body: some View {
-        Text("\(employee.nombre)")
+        VStack {
+            Spacer()
+            Image(systemName: "person")
+                .resizable()
+                .frame(width: 100, height: 100)
+            HStack {
+                Spacer()
+                Text("\(employee.nombre) \(employee.apell1)")
+                
+                if let apell2 = employee.apell2 {
+                    Text("\(apell2)")
+                }
+                Spacer()
+            }
+            Spacer()
+        }
+        .padding()
+        .background(Color.gray)
+        .cornerRadius(25)
+        .foregroundStyle(.white)
+        
     }
 }
 
+/*
 #Preview {
     EmpleadoRowView(employee: Empleados(id: "100", nombre: "Daniel", apell1: "Cazorro", apell2: "San", idPhoto: "02", edad: 22))
         .previewLayout(.fixed(width: 400.0, height: 200.0))
+}
+*/
+//code 14 y anterior
+struct EmpleadoRowView_Previews: PreviewProvider {
+    static var previews: some View{
+        EmpleadoRowView(employee: Empleados(id: "100", nombre: "jose luis", apell1: "bustos", apell2: "Esteban", idPhoto: "091", edad: 22))
+            .previewLayout(.fixed(width: 400.0, height: 200.0))
+    }
 }
--- iOS Super Powers/kcMultiplataforma2023/kcMultiplataforma2023/Models/Empleados.swift
+++ iOS Super Powers/kcMultiplataforma2023/kcMultiplataforma2023/Models/Empleados.swift
@@ -11,7 +11,7 @@ struct Empleados: Identifiable {
     let id: String
     let nombre: String
     let apell1: String
-    let apell2: String
+    let apell2: String?
     let idPhoto: String
     let edad: Int
 }
