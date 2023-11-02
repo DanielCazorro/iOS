03699f6786ed6c3479b7a45b48382e6e89fac443
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-11-02 22:05:34 +0100

    AsyncAwait

--- iOS Super Powers/Playground.AsyncAwait.playground/Pages/02.Combine.xcplaygroundpage/Contents.swift
+++ iOS Super Powers/Playground.AsyncAwait.playground/Pages/02.Combine.xcplaygroundpage/Contents.swift
@@ -1,6 +1,6 @@
 /*
  
- Closures
+ Combine
  
  */
 import Foundation
--- iOS Super Powers/Playground.AsyncAwait.playground/Pages/03.AsyncAwait.xcplaygroundpage/Contents.swift
+++ iOS Super Powers/Playground.AsyncAwait.playground/Pages/03.AsyncAwait.xcplaygroundpage/Contents.swift
@@ -1,7 +1,26 @@
-//: [Previous](@previous)
-
+/*
+ 
+ Async Await
+ 
+ */
 import Foundation
+import Combine
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
 
-var greeting = "Hello, playground"
+// Modelo de BootCamps
+struct BootCamps: Codable {
+    let id: UUID
+    let name: String
+}
 
-//: [Next](@next)
+
+// Extensión de URLResponse
+extension URLResponse {
+    func getStatusCode() -> Int? {
+        if let httpResponse = self as? HTTPURLResponse {
+            return httpResponse.statusCode
+        }
+        return nil
+    }
+}
