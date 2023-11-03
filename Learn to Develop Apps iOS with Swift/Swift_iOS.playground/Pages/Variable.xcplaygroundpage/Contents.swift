54e43ef8c090e58ceb3086f1486c01c259cd00a5
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-11-03 10:07:48 +0100

    Very Simple variable and constant

---
+++ Learn to Develop Apps iOS with Swift/Swift_iOS.playground/Pages/Variable.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// Definiendo una variable -> var nombreVariable: TipoDato
+var letter: String
+letter = "I'm learning Swift"
+
+// Definiendo una constante -> let nombreConstante: TipoDato
+let letterApril: String
+letterApril = "This letter is constant!"
+// letterApril = "You can't change a constant"
