b5c7616059cb35fdfc8dca7fa396d24c30fadd69
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-11-03 10:49:02 +0100

    Basic Operations

--- Learn to Develop Apps iOS with Swift/Swift_iOS.playground/Pages/Variable.xcplaygroundpage/Contents.swift
+++ Learn to Develop Apps iOS with Swift/Swift_iOS.playground/Pages/Variable.xcplaygroundpage/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+//MARK: - 2.- Variables y Tipos -
+
 // Definiendo una variable -> var nombreVariable: TipoDato
 var letter: String
 letter = "I'm learning Swift"
@@ -8,3 +10,30 @@ letter = "I'm learning Swift"
 let letterApril: String
 letterApril = "This letter is constant!"
 // letterApril = "You can't change a constant"
+
+
+// String, Int, Float, Double, Bool
+var numberOfLetters: UInt = 10
+numberOfLetters = 0
+
+var age: Int = 35
+age = -1
+
+var letterPrice: Float = 1.29
+
+var letterBigPrice: Double = 1.29000001
+
+var isLetterSended: Bool = true
+isLetterSended = false
+
+
+var ageSum = 18 + 7
+ageSum = age + 18
+
+var letterFinalPrice = Double(letterPrice) + letterBigPrice
+var letterFinalPrice2 = letterPrice + Float(letterBigPrice)
+
+// Operaciones básicas: multipliación *, división /, suma +, resta -, módulo %
+
+var division = 10 / 3
+var modulo = 10 % 3
