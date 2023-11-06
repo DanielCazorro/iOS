fe7d0d484ce764446383122e682680d1b84f93a9
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-11-06 19:10:57 +0100

    New Proyect: SwiftUI

---
+++ iOS Advanced/kcHerosClase/kcHerosClase/kcHerosClaseApp.swift
@@ -0,0 +1,20 @@
+//
+//  kcHerosClaseApp.swift
+//  kcHerosClase
+//
+//  Created by Daniel Cazorro Frías on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct kcHerosClaseApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
