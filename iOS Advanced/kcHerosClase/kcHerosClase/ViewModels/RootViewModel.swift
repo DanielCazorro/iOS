23921e773ec4b08e8203b85ab4d9cdbe7d4059ac
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-11-06 21:37:07 +0100

    Login

--- iOS Advanced/kcHerosClase/kcHerosClase/ViewModels/RootViewModel.swift
+++ iOS Advanced/kcHerosClase/kcHerosClase/ViewModels/RootViewModel.swift
@@ -18,5 +18,39 @@ final class RootViewModel: ObservableObject {
     // Combine
     var suscriptors = Set<AnyCancellable>()
     
+    // Login al servidor
+    func login(user:String, password: String) {
+        
+        status = .loading
+        
+        URLSession.shared
+            .dataTaskPublisher(for: BaseNetwork().getSessionLogin(user: user, password: password))
+            .tryMap{
+                // Evaluamos si es 200, sino lo es: Exception. Si es 200, devolvemos el JSON, que es el data
+                guard let response = $0.response as?  HTTPURLResponse,
+                      response.statusCode == 200 else {
+                    //Error
+                    throw URLError(.badServerResponse)
+                }
+                
+                // Todo OK. Convierto el Data -> Cadena usando codificación utf8
+                return String(decoding: $0.data, as: UTF8.self)
+            }
+            .receive(on: DispatchQueue.main) // Hilo principal
+            .sink { completion in
+                // Evaluamos la respuesta
+                switch completion {
+                case .failure:
+                    self.status = .error(error: "Usuario y/o clave incorrecta") // Login Error
+                case .finished:
+                    self.status = .loaded // Login OK
+                }
+                
+            } receiveValue: { token in
+                self.tokenJWT = token
+            }
+            .store(in: &suscriptors)
+        
+    }
     
 }
