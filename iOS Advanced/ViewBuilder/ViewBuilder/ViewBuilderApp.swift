508718d7145019f9356ed6b03daa9f0405fed5be
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-11-07 20:00:29 +0100

    ViewBuilder

---
+++ iOS Advanced/ViewBuilder/ViewBuilder/ViewBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewBuilderApp.swift
+//  ViewBuilder
+//
+//  Created by Daniel Cazorro Frías on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
