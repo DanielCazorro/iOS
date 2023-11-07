fc64deb417e185e30b9bc76368eea29663fda1f7
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-11-07 19:24:18 +0100

    HerosView

--- iOS Advanced/kcHerosClase/kcHerosClase/View/Home/Heros/HerosView.swift
+++ iOS Advanced/kcHerosClase/kcHerosClase/View/Home/Heros/HerosView.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 
 struct HerosView: View {
+    @StateObject var viewModel: viewModelHeros
+    @State private var filter: String = ""
+    
     var body: some View {
         Text("Hero List")
     }
 }
 
 #Preview {
-    HerosView()
+    HerosView(viewModel: viewModelHeros(testing: true))
 }
