1e3428e3c6ae84b639e13065e9d470378b5fcac9
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-11-07 19:31:44 +0100

    Heros View, HerosRowView

--- iOS Advanced/kcHerosClase/kcHerosClase/View/Home/Heros/HerosView.swift
+++ iOS Advanced/kcHerosClase/kcHerosClase/View/Home/Heros/HerosView.swift
@@ -12,9 +12,21 @@ struct HerosView: View {
     @State private var filter: String = ""
     
     var body: some View {
-        Text("Hero List")
+            NavigationView {
+                List(viewModel.heros, id: \.name) { data in
+                    NavigationLink(destination: Text("Detalles de \(data.name)")) {
+                        Text("\(data.name)")
+                    }
+                }
+            }
+        }
     }
-}
+
+    struct HerosView_Previews: PreviewProvider {
+        static var previews: some View {
+            HerosView(viewModel: viewModelHeros(testing: true))
+        }
+    }
 
 #Preview {
     HerosView(viewModel: viewModelHeros(testing: true))
