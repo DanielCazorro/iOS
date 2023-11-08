a8a17c606e5abeb86f4bb6c0f12cd72f4a690ba4
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-11-08 20:41:24 +0100

    Making Buttons

--- iOS Super Powers/SwiftUIinUIKit/SwiftUIinUIKit/ViewController.swift
+++ iOS Super Powers/SwiftUIinUIKit/SwiftUIinUIKit/ViewController.swift
@@ -13,7 +13,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func openSwiftUIView(_ sender: Any) {
+    }
+    
 
-
+    @IBAction func openTableViewController(_ sender: Any) {
+    }
 }
 
