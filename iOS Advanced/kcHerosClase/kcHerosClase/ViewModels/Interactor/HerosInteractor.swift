f38c254cfc760bc88169a3a38130b058636ce0f5
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-11-08 20:25:31 +0100

    Heros Interactor

---
+++ iOS Advanced/kcHerosClase/kcHerosClase/ViewModels/Interactor/HerosInteractor.swift
@@ -0,0 +1,32 @@
+//
+//  HerosInteractor.swift
+//  kcHerosClase
+//
+//  Created by Daniel Cazorro Frías on 8/11/23.
+//
+
+import Foundation
+import Combine
+
+protocol HerosInteractorProtocol: AnyObject {
+    func getHeros(filter: String) -> AnyPublisher<[Heros], Error>
+}
+
+final class HerosInteractor: HerosInteractorProtocol {
+    func getHeros(filter: String) -> AnyPublisher<[Heros], Error> {
+        return networker.callServer(type: [Heros].self, request: baseNetwork.getSessionHero(filter: filter))
+    }
+    
+
+    
+    let networker: NetworkerProtocol // Genérico
+    let baseNetwork: BaseNetwork
+    
+    init(network: NetworkerProtocol = Networker(), baseNetwork: BaseNetwork = BaseNetwork()){
+        self.networker = network
+        self.baseNetwork = baseNetwork
+        
+    }
+    
+    
+}
