82a9ade83b7e1dfd18659dfdebe2d17ae6209cdd
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-11-09 20:05:16 +0100

    TableViewController fix

--- iOS Super Powers/SwiftUIinUIKit/SwiftUIinUIKit/TableView/TableViewControllerTest.swift
+++ iOS Super Powers/SwiftUIinUIKit/SwiftUIinUIKit/TableView/TableViewControllerTest.swift
@@ -9,27 +9,27 @@ import UIKit
 import SwiftUI
 
 class TableViewControllerTest: UITableViewController {
-
+    
     private let datos = [1,2,3,4,5,6,7,8,9,10]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Registrar las celdas ... SIEMPRE
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return datos.count
     }
-
+    
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if indexPath.row == 0 {
             return 250
@@ -40,22 +40,22 @@ class TableViewControllerTest: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
+        
         // Extraer el modelo
         let item = datos[indexPath.row]
         
         cell.contentConfiguration = UIHostingConfiguration{
             // Aquí el SwiftUI
             /*
-            VStack{
-                Text("Item \(item)")
-            }
-            .font(.title)
-            .background(.blue)
+             VStack{
+             Text("Item \(item)")
+             }
+             .font(.title)
+             .background(.blue)
              */
             CellRowView(numero: item)
         }
-
+        
         return cell
     }
 }
