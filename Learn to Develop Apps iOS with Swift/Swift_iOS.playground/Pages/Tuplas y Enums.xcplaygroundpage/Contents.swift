78cdf779c72a02141c3817fdef49e41a884f700e
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-11-22 12:47:35 +0100

    Tupla examples

--- Learn to Develop Apps iOS with Swift/Swift_iOS.playground/Pages/Tuplas y Enums.xcplaygroundpage/Contents.swift
+++ Learn to Develop Apps iOS with Swift/Swift_iOS.playground/Pages/Tuplas y Enums.xcplaygroundpage/Contents.swift
@@ -1,5 +1,14 @@
 import Foundation
 
+/** Tuplas y Enum **/
+
+
 // MARK: Tuplas
 
-let mailbox: (adress: String, letters: Int)
+let mailbox = ("Keepcoding", 10)
+mailbox.0
+mailbox.1
+
+let mailboxAlias: (name:String, number: Int) = ("Keepcoding", 10)
+mailboxAlias.name
+mailboxAlias.number
