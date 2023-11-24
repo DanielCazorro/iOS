7ca61c99b878ddf64eca84cebb65de9746948c38
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-11-24 15:56:04 +0100

    Basic if Theory

---
+++ Learn to Develop Apps iOS with Swift/Swift_iOS.playground/Pages/Sentencias de Control.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+/** Sentencias de Control **/
+
+let lettersCount = 5
+
+if(lettersCount >= 5) {
+    print("Tienes muchas cartas en el buzón")
+} else if(lettersCount > 0) {
+    print("Tienes algunas cartas en el buzón")
+} else {
+    print("No tienes cartas en el buzón")
+}
+
